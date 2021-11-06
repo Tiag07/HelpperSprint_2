1f0341fa753845a72c5ef6c34d4f5e2335e79fdb
Tiago Rodrigues <tiag07desa@gmail.com>
2021-11-06 11:54:48 -0300

    operator

--- firstCode.ts
+++ firstCode.ts
@@ -1,5 +1,30 @@
-function soma(numA : number, numB : number) : number{
-    return numA + numB;
+enum EnumOperator{
+    sum, subtraction, division, multiply
 }
 
-console.log(soma(3, 13));
+function calculator(numA : number, numB : number, operation : EnumOperator) : number{
+    switch(operation){
+
+        case EnumOperator.sum:
+            return numA + numB;
+            break;
+
+        case EnumOperator.subtraction:
+            return numA - numB;
+            break;
+
+            case EnumOperator.division: 
+            return numA/numB;
+            break;
+
+            default:
+                return numA * numB;
+                break;
+    }
+    
+}
+
+console.log(calculator(3, 13, EnumOperator.sum));
+console.log(calculator(3, 13, EnumOperator.subtraction));
+console.log(calculator(3, 13, EnumOperator.division));
+console.log(calculator(3, 13, EnumOperator.multiply));
