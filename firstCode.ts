2cd04e6a007be98ed80613c06d58cca85756e530
Tiago Rodrigues <tiag07desa@gmail.com>
2021-11-06 12:10:22 -0300

    rest

--- firstCode.ts
+++ firstCode.ts
@@ -1,28 +1,33 @@
 enum EnumOperator{
-    sum, subtraction, division, multiply
+    sum, subtraction, division, multiply, rest
 }
 
 function calculator(numA : number, numB : number, operation : EnumOperator) : number{
     switch(operation){
 
         case EnumOperator.sum:
-            console.log("O resultado da operação é: ");
+            console.log("O resultado da operação será: ");
             return numA + numB;
             break;
 
         case EnumOperator.subtraction:
-            console.log("O resultado da operação é: ");
+            console.log("O resultado da operação será: ");
             return numA - numB;
             break;
 
             case EnumOperator.division: 
-            console.log("O resultado da operação é: ");
+            console.log("O resultado da operação será: ");
             return numA/numB;
             break;
 
-            default:
-                console.log("O resultado da operação é: ");
+            case EnumOperator.multiply:
+                console.log("O resultado da operação será: ");
                 return numA * numB;
+                break;
+
+            case EnumOperator.rest:
+                console.log("O resultado da operação será: ");
+                return numA % numB;
                 break;
     }
 
@@ -32,3 +37,4 @@ console.log(calculator(3, 13, EnumOperator.sum));
 console.log(calculator(3, 13, EnumOperator.subtraction));
 console.log(calculator(3, 13, EnumOperator.division));
 console.log(calculator(3, 13, EnumOperator.multiply));
+console.log(calculator(3, 13, EnumOperator.rest));
