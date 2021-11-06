690890d91ad66db465f60a9592ab0553df01ea94
Tiago Rodrigues <tiag07desa@gmail.com>
2021-11-06 11:47:53 -0300

    soma

---
+++ firstCode.js
@@ -0,0 +1,5 @@
+"use strict";
+function soma(numA, numB) {
+    return numA + numB;
+}
+console.log(soma(3, 13));
---
+++ firstCode.ts
@@ -0,0 +1,5 @@
+function soma(numA : number, numB : number) : number{
+    return numA + numB;
+}
+
+console.log(soma(3, 13));
